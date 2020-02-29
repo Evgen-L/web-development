1d962c9bc90f7ba72c600cba89253996dfc19fef
Evgen-L <Evgen-L@users.noreply.github.com>
2020-02-29 07:16:24 +0300

    lol

---
+++ Lw3/task 2/CheckIdentifier.php
@@ -0,0 +1,31 @@
+<?php
+	function checkIdentifier(string $identifier):?string
+	{
+	     $strNew = $_GET["identifier"];
+		 $checkInd = 'Yes';
+		 $i = 1;
+		 if (($strNew[0] >= '0') AND ($strNew[0] <= '9'))
+		 {
+			echo(($strNew[0]). "Первый символ должен быть буквой</br>");
+			$checkInd = 'No';
+		 }	
+		 while ($i < strlen($strNew))
+		 {
+			if ((($strNew[$i] < 'A') or ($strNew[$i] > 'Z')) and (($strNew[$i] < 'a') or ($strNew[$i] > 'z')) and (($strNew[$i] < '0') or ($strNew[$i] > '9')))
+			{
+			    $chekInd = 'No';
+				echo ($strNew[$i]. "Этот символ недопустим </br>");
+			}
+			$i++;
+		 }
+		 if ($checkInd === 'No')
+		 {
+			$strNew = $strNew. "can be identifier";
+		 }else
+		  {
+		       echo "</br>";
+			   $strNew = $strNew. " is identifier";
+		  }
+		 return $strNew;
+	}
+	echo checkIdentifier('');
