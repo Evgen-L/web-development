b7fe2167ef6925c0d7a7713bb6626e8ddaa5937e
Evgen-L <Evgen-L@users.noreply.github.com>
2020-03-29 22:05:16 +0300

    исправлены ошибки форматирования

--- Lw3/task3/PasswordStrength.php
+++ Lw3/task3/PasswordStrength.php
@@ -1,62 +1,61 @@
 <?php
-	function passwordSafety()
-	{
-	     $strNew = $_GET["password"];
-		 $duplicateСharacters = $strNew;
-		 $countDuplicateCharacters = 1;
-		 $countCapitalLetters = 0;
-		 $countLowerCase = 0;
-		 $countNumbers = 0;
-         $passwordSafety = 0;
-		 $i = 0;
-		 while ($i <= strlen($strNew))
-		 {
+    function passwordSafety()
+    {
+        $strNew = $_GET["password"];
+        $duplicateСharacters = $strNew;
+        $countDuplicateCharacters = 1;
+        $countCapitalLetters = 0;
+        $countLowerCase = 0;
+        $countNumbers = 0;
+        $passwordSafety = 0;
+        $i = 0;
+        while ($i <= strlen($strNew))
+        {
             if (($strNew[$i] >= 'A') and ($strNew[$i] <= 'Z')) 
-			{
+            {
                 $passwordSafety = $passwordSafety + 4;
-				$countCapitalLetters++;
-			}
-			if (($strNew[$i] >= 'a') and ($strNew[$i] <= 'z')) 
-			{
+                $countCapitalLetters++;
+            }
+            if (($strNew[$i] >= 'a') and ($strNew[$i] <= 'z')) 
+            {
                 $passwordSafety = $passwordSafety + 4;
-				$countLowerCase++;
-			}
-			if (($strNew[$i] >= '0') and ($strNew[$i] <= '9')) 
-			{
-				$countNumbers++;
-				$countAllSymbols++;
-				$passwordSafety = $passwordSafety + 8;
-			}
-			$j = 0;
-			while ($j <= strlen($duplicateСharacters))
-			{
-
-		        if (($strNew[$i] === $duplicateСharacters[$j]) and ($i != $j))  
-			     {
-			         $countDuplicateCharacters++;
-				     $duplicateСharacters[$j] = ' '; 
-			     }
+                $countLowerCase++;
+            }
+            if (($strNew[$i] >= '0') and ($strNew[$i] <= '9')) 
+            {
+                $countNumbers++;
+                $countAllSymbols++;
+                $passwordSafety = $passwordSafety + 8;
+            }
+            $j = 0;
+            while ($j <= strlen($duplicateСharacters))
+            {
+                if (($strNew[$i] === $duplicateСharacters[$j]) and ($i != $j))  
+                {
+                    $countDuplicateCharacters++;
+                    $duplicateСharacters[$j] = ' '; 
+                }
                 $j++;
-			}	
-			$i++;
-		 }
-		 if (($countNumbers === 0) or (($countLowerCase === 0) and ($countCapitalLetters === 0)))
-		 {
-			$passwordSafety = $passwordSafety - strlen($strNew);
-		 } 
-		 if ($countDuplicateCharacters > 1)
-		 {
-	        $passwordSafety = $passwordSafety - $countDuplicateCharacters + 1;
-		 }	
-         if ($countCapitalLetters > 0)
-         {
-             $passwordSafety = $passwordSafety + ((strlen($strNew) - $countCapitalLetters) * 2);
-         }
-         if ($countLowerCase > 0)
-         {
-             $passwordSafety = $passwordSafety + ((strlen($strNew) - $countLowerCase) * 2);
-         }
-		 return $passwordSafety;
-	}
-	echo passwordSafety();
+            }
+            $i++;
+        }
+        if (($countNumbers === 0) or (($countLowerCase === 0) and ($countCapitalLetters === 0)))
+        {
+            $passwordSafety = $passwordSafety - strlen($strNew);
+        } 
+        if ($countDuplicateCharacters > 1)
+        {
+            $passwordSafety = $passwordSafety - $countDuplicateCharacters + 1;
+        }
+        if ($countCapitalLetters > 0)
+        {
+            $passwordSafety = $passwordSafety + ((strlen($strNew) - $countCapitalLetters) * 2);
+        }
+        if ($countLowerCase > 0)
+        {
+            $passwordSafety = $passwordSafety + ((strlen($strNew) - $countLowerCase) * 2);
+        }
+        return $passwordSafety;
+    }
+    echo passwordSafety();
 ?>
